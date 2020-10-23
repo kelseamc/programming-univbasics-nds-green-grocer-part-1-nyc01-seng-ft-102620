5a0552bcf902ae2face4b140604487d691c6a934
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-23 17:37:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,8 +13,9 @@ def consolidate_cart(cart)
   scanned_cart= []
   cart.each do |cart_hash|
     scanned_cart.each do |scan_hash|
-      if scan_hash == find_item_by_name_in_collection(cart_hash[:item], scanned_cart)
-          scan_hash[:count] += 1
+      item = find_item_by_name_in_collection(cart_hash[:item], scanned_cart)
+      if item 
+          item[:count] += 1
       else
         cart_hash[:count] = 1
         scanned_cart << cart_hash
